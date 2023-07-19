289376a80ff8c5e3279b0f18be105f016f5d6fce
Erban Danaj <danajerban@gmail.com>
2023-07-19 16:15:43 +0200

    -CodeWorks Legaxy Project Clean Up

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+const inter = Inter({ subsets: ["latin"] });
+import { dark } from "@clerk/themes";
+export const metadata = {
+  title: "Explore the Aroma",
+  description: "Legacy project",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+        appearance={{
+          baseTheme: dark,
+          variables: {
+            colorPrimary: "#bc987e",
+          }
+
+        }}
+      >
+      <html lang="en">
+        <body className='all'>{children }</body>
+      </html>
+    </ClerkProvider>
+  );
+}
